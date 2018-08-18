ec6e908d6c9ba6186f49c3e45d94a2b9836e6754
Nicolas <nikola.olg@gmail.com>
2018-08-18 23:25:19 +0200

    reinit project

---
+++ backend/src/main/java/my/MyController.java
@@ -0,0 +1,15 @@
+package my;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "TestController";
+    }
+
+
+}
